2a1a298301468e3dc517133f8b43f8ae02f1bddb
Code Savant <162926311+Charcoal00@users.noreply.github.com>
2024-07-21 18:31:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+//privacy & policy
+function openBar() {
+    document.getElementById("containerr").classList.toggle("open-now");
+    document.querySelector(".about-container").classList.toggle("open");
+}
+
+
+function openBar2() {
+    document.getElementById("servicee").classList.toggle("open-now");
+    document.querySelector(".service-container").classList.toggle("open2");
+}
+
+
+
+
+// price buttons
+
+
+
+document.querySelectorAll('.price').forEach(button => {
+
+    button.addEventListener
+    ('click', function() {
+        
+            // Get the span ID from the data attribute
+            const spanId = this.getAttribute('data-span-id');
+            // Add the 'highlight' class to the corresponding span element
+            document.getElementById(spanId).classList.add('open-now');
+
+        }
+    );
+});
